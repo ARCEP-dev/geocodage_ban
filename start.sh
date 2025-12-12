726a4e4ec660c90a9f140d632f0966473f6b5efc
Laurent COTTEREAU <laurent.cottereau@arcep.fr>
2025-12-12 18:16:58 +0100

    Montée de version traefik et fusion des compose.yml
    - définit une version spécifique de traefik pour choisir quand on met à jour
    - fusionne les 2 compose en 1 seul (traefik + addok)
    - supprime le script de mise à jour des données (qui est fait désormais par https://gitlab.arcep.fr/urd/reference/mise-a-jour-ban
    - supprime les confs par défaut (qu'on n'utilisait pas de toute façon, et qui changeaient avec les versions)
    - prend en compte la montée de version de traefik (configuration par exemple), voir https://doc.traefik.io/traefik/reference/install-configuration/providers/docker/
    - prend en compte la montée de version de docker-compose (plus de version dans le yml)

--- start.sh
+++ start.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 # params : nbr_redis nbr_addok
-
-
-echo "Lancement de l'instance de géocodage"
-
-echo "Lancement de traefik"
-docker-compose -f traefik/traefik-compose.yml up -d
-
 echo "Lancement d'addok"
-docker-compose -f addok/addok-compose.yml up --scale addok=$2 --scale addok-redis=$1 -d
+docker-compose up --scale addok=$2 --scale addok-redis=$1 -d
